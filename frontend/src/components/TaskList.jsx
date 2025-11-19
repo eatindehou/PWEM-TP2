9064adedaea34c133913e76dce89814ff838a256
Jade Mayrand <1942810@csfoy.ca>
2025-11-19 15:46:46 -0500

    formulaire ajout tâche

---
+++ frontend/src/components/TaskList.jsx
@@ -0,0 +1,7 @@
+function TaskList() {
+ return (
+     <div class="todo-list"></div>
+  );
+}
+
+export default TaskList;
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import TaskForm from '../components/TaskForm'
+import TaskList from '../components/TaskList'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
